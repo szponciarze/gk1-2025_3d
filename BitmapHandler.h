e7630c6bc334e5098c98752b74a6c58348e6bc70
dexter3679 <dominik.szczesniak.ciech@gmail.com>
2026-01-25 19:15:24 +0100

    Dodanie komentarzy do doxygena

--- BitmapHandler.h
+++ BitmapHandler.h
@@ -1,9 +1,31 @@
+/**
+ * @file BitmapHandler.h
+ * @brief Klasa narzêdziowa do obs³ugi plików graficznych i tekstur OpenGL.
+ */
+
 #pragma once
 #include "glew/include/GL/glew.h"
 #include <string>
 
+ /**
+  * @class BitmapHandler
+  * @brief Statyczna klasa odpowiedzialna za ³adowanie obrazów i tworzenie tekstur OpenGL.
+  *
+  * Klasa wykorzystuje bibliotekê stb_image do odczytu danych binarnych z plików
+  * i przesy³ania ich do pamiêci karty graficznej.
+  */
+
 class BitmapHandler {
 public:
+    /**
+   * @brief £aduje plik graficzny i tworzy z niego teksturê OpenGL.
+   *
+   * Funkcja automatycznie odwraca obraz pionowo (flip), generuje mipmapy
+   * oraz ustawia parametry filtrowania na GL_LINEAR.
+   *
+   * @param filename Œcie¿ka do pliku graficznego.
+   * @return GLuint Identyfikator wygenerowanej tekstury OpenGL. Zwraca 0 w przypadku b³êdu.
+   */
  
     static GLuint loadTexture(const std::string& filename);
 };
--- TexturedCube.h
+++ TexturedCube.h
@@ -1,11 +1,42 @@
+/**
+ * @file TexturedCube.h
+ * @brief Klasa reprezentuj¹ca teksturowany model szeœcianu.
+ */
+
 #pragma once
 #include "Primitive.h"
 
+ /**
+  * @class TexturedCube
+  * @brief Klasa definiuj¹ca szeœcian z na³o¿on¹ tekstur¹.
+  *
+  * Dziedziczy po klasie Primitive i implementuje specyficzne dla szeœcianu
+  * buforowanie wierzcho³ków oraz nak³adanie wspó³rzêdnych tekstur.
+  */
+
 class TexturedCube : public Primitive {
 private:
+    /** @brief Wektor indeksów wierzcho³ków do renderowania */
     std::vector<GLuint> indices;
+    /** @brief Identyfikator tekstury OpenGL przypisanej do szeœcianu */
     GLuint textureID;
 public:
+    /**
+   * @brief Konstruktor tworz¹cy model szeœcianu.
+   *
+   * Inicjalizuje geometriê szeœcianu o zadanych wymiarach i przypisuje teksturê.
+   *
+   * @param size D³ugoœæ boku szeœcianu.
+   * @param texID Identyfikator za³adowanej wczeœniej tekstury OpenGL.
+   */
+
     TexturedCube(float size, GLuint texID);
+
+    /**
+     * @brief Renderuje szeœcian na ekranie.
+     *
+     * Nadpisuje metodê draw z klasy bazowej. Odpowiada za bindowanie tekstury
+     * oraz wywo³anie poleceñ rysuj¹cych OpenGL.
+     */
     void draw() override;
 }; 
